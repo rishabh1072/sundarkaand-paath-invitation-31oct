d069808fca331c63598534eed382a585a6260909
Rishabh Agrawal <Rishabh.Agrawal@target.com>
2025-09-21 16:31:44 +0530

    fixing git page issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/hanuman-aashirwad-site/', // <-- Added for GitHub Pages
   server: {
     host: "::",
     port: 8080,

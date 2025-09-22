624420c0ea04697d2873a5cab540ab8b4059bbe8
Rishabh Agrawal <Rishabh.Agrawal@target.com>
2025-09-22 12:08:17 +0530

    adding icon file

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <LanguageProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/hanuman-invitation' : ''}>
+        <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/hanuman-aashirwad-site' : ''}>
           <Routes>
             <Route path="/" element={<MainPage />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
